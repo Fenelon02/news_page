f8ba01d0ffe3237a281a7b6cf28b84a972f29a94
Fenelon02 <joaofenelonramossoares@gmail.com>
2025-11-16 16:04:41 -0300

    create the from file - not finished

---
+++ src/components/ui/Form.jsx
@@ -0,0 +1,24 @@
+import { useForm } from 'react-hook-form';
+
+const Form = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+  console.log(errors);
+  
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      
+      <select {...register("Title", { required: true }) }>
+        <option value="Mr">Mr</option>
+        <option value="Mrs">Mrs</option>
+        <option value="Miss">Miss</option>
+        <option value="Dr">Dr</option>
+      </select>
+
+      <input {...register("Developer", { required: true })} type="radio" value="Yes" />
+      <input {...register("Developer", { required: true })} type="radio" value="No" />
+
+      <input type="submit" />
+    </form>
+  );
+}
