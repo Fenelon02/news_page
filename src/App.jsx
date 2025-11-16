9621ff230df3862167978ef3fa54d05105e9df2f
Fenelon02 <joaofenelonramossoares@gmail.com>
2025-11-16 16:01:19 -0300

    initializing to development the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Home from './components/Home'
 
 function App() {
 
   return (
     <div>
-      <h1 className="text-blue-700 text-5xl">teste</h1>
+      <Home/>
     </div>
   )
 }
