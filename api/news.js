ca946322888d8cbb20b43ab42fcf1ca77f562937
Fenelon02 <joaofenelonramossoares@gmail.com>
2025-11-17 13:13:36 -0300

    update api request for production

--- api/news.js
+++ api/news.js
@@ -4,19 +4,29 @@ const ENVIROMENT = import.meta.env.ENVIROMENT;
 let API_KEY;
 
 if (ENVIROMENT == "Production") {
-  API_KEY = import.meta.env.GNEWS_API_KEY  
+  API_KEY = process.env.GNEWS_API_KEY  
 } else {
   API_KEY = import.meta.env.VITE_GNEWS_API_KEY
 }
 
-export default async function handler(country, lang) { 
+export default async function handler(request, response) { 
   try {
-    const response = await axios.get(
+    const { country, lang } = request.query;
+
+    if (!API_KEY) {
+      return response.status(500).json({ error: "API Key não configurada no servidor" });
+    }
+
+    const apiResponse = await axios.get(
       `https://gnews.io/api/v4/top-headlines?category=world&lang=${lang}&country=${country}&max=10&apikey=${API_KEY}`,
     );
-    console.log("macaco")
-    return response.data.articles
+    
+    console.log("macaco");
+
+
+    return response.status(200).json(apiResponse.data.articles);
+
   } catch (error) {
-    return error
+    return response.status(500).json({ error: "Falha ao buscar notícias", details: error.message });
   }
 }
