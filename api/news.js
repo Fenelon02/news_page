2e90f51d0e4f67efd8e4c9c2777ce2a20964fef8
Fenelon02 <joaofenelonramossoares@gmail.com>
2025-11-17 12:27:06 -0300

    fix request api

--- api/news.js
+++ api/news.js
@@ -1,19 +1,14 @@
 import axios from "axios";
-const api_key = process.env.GNEWS_API_KEY;
 
-export default async function handler(req, res) { 
-  
-  const { country, lang } = req.query; 
-  if (!country || !lang || !api_key) {
-    return res.status(400).json({ error: "Parâmetros faltando ou API Key não configurada" });
-  }
+const API_KEY = import.meta.env.VITE_GNEWS_API_KEY
 
+export default async function handler(country, lang) { 
   try {
     const response = await axios.get(
-      `https://gnews.io/api/v4/top-headlines?category=world&lang=${lang}&country=${country}&max=10&apikey=${api_key}`,
+      `https://gnews.io/api/v4/top-headlines?category=world&lang=${lang}&country=${country}&max=10&apikey=${API_KEY}`,
     );
-    return res.status(200).json(response.data.articles);
+    return response.data.articles
   } catch (error) {
-    return res.status(500).json({ error: "Erro ao buscar notícias" });
+    return error
   }
 }
