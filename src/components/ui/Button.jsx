fcff07b0c6b86d8755fe234453b3a795b68220ae
Fenelon02 <joaofenelonramossoares@gmail.com>
2025-11-24 18:28:17 -0300

    create an component for buttons (not finished)

---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,28 @@
+const Button = (props) =>{
+    const variants = {
+        primary: "bg-blue-500 text-white hover:bg-blue-700 focus:bg-blue-700 focus:rounded-b-xl hover:rounded-b-xl",
+        secondary: "bg-gray-500 text-white hover:bg-gray-600 focus:bg-blue-600",  
+    }
+    const width = {
+        full: "w-full",
+        small: "w-32",
+        medium: "w-48",
+        large: "w-64"
+    }
+
+    const btnClasses = [
+        variants[props.variant] || variants.primary,
+        width[props.width] || variants.full
+    ].join(" ")
+
+    return (
+        <div>
+            <button className={`${btnClasses} ${props.className || ""}` } onClick={props.onClick}>
+                {props.icon}
+                {props.children}
+            </button>
+        </div>
+    )
+}
+
+export default Button
