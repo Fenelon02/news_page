3d8ef58579248cbb4d4274358ddf5667f90fb6eb
Fenelon02 <joaofenelonramossoares@gmail.com>
2025-11-26 10:57:38 -0300

    fix: the style of the component

--- src/components/ui/Select.jsx
+++ src/components/ui/Select.jsx
@@ -3,7 +3,7 @@ import { useFormContext } from "react-hook-form";
 const Select = ({options, selectName, required}) => {
     const { register } = useFormContext();
     return(
-        <select {...register(`${selectName}`, { required: {required} })} className="w-40 rounded-lg bg-amber-100 mr-2 px-2 h-8 focus:shadow-lg focus:shadow-white hover:outline-2 hover:outline-amber-300">
+        <select {...register(`${selectName}`, { required: {required} })} className="w-40 rounded-lg bg-amber-100 mr-2 px-2 h-8 hover:shadow-md hover:shadow-amber-100 focus:outline-0">
             {options.map((option) => (
                 <option key={option.value} value={option.value} >{option.label}</option>
             ))}
