7f8f7758215fcf8040d8af71be20d34de5209cfb
Fenelon02 <joaofenelonramossoares@gmail.com>
2025-11-26 10:58:25 -0300

    fix: the variants style and adding the type props of the button

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,7 +1,7 @@
 const Button = (props) =>{
     const variants = {
-        primary: "bg-blue-500 text-white hover:bg-blue-700 focus:bg-blue-700 focus:rounded-b-xl hover:rounded-b-xl",
-        secondary: "bg-gray-500 text-white hover:bg-gray-600 focus:bg-blue-600",  
+        primary: "bg-blue-500 text-white hover:bg-blue-700 focus:bg-blue-700 focus:rounded-b-xl hover:rounded-b-xl mr-2 px-2 h-8 w-15 flex flex-col items-center justify-center",
+        header:"rounded-lg bg-amber-100 mr-2 px-2 h-8 focus:shadow-lg focus:shadow-white hover:shadow-md hover:shadow-amber-100 flex flex-col items-center justify-center",  
     }
     const width = {
         full: "w-full",
@@ -17,7 +17,7 @@ const Button = (props) =>{
 
     return (
         <div>
-            <button className={`${btnClasses} ${props.className || ""}` } onClick={props.onClick} key={props.id}>
+            <button className={`${btnClasses} ${props.className || ""}` } onClick={props.onClick} key={props.id} type={props.type || "button"}>
                 {props.icon}
                 {props.children}
             </button>
