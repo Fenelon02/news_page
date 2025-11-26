013d92b037c43c71fe54f568c9872f3b61a40585
Fenelon02 <joaofenelonramossoares@gmail.com>
2025-11-26 10:56:53 -0300

    Fix: the button on the header

--- src/components/cards/Header.jsx
+++ src/components/cards/Header.jsx
@@ -78,11 +78,16 @@ const Header = ({onSearch}) => {
                                     required={true}
                                 />
             
-                                <button type="submit" className="bg-amber-100 rounded-lg mr-2 px-2 h-8 w-15 flex flex-col items-center justify-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
-                                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-                                    </svg>
-                                </button>
+                                <Button 
+                                    type="submit" 
+                                    className="bg-amber-100 h-full" 
+                                    variant="header"
+                                    width="full"
+                                    children={<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                                              </svg>}
+                                />
+                                    
                             </form>
                         </FormProvider>
                 </div>
