23632fb57e222bf297004d7e61bd88541a39e2de
Fenelon02 <joaofenelonramossoares@gmail.com>
2025-11-27 16:20:30 -0300

    FIX: the name of the file

---
+++ src/components/cards/TopicNavigation.jsx
@@ -0,0 +1,35 @@
+import Button from "../ui/Button";
+
+const CATEGORIES = [
+    { value: "general", label: "Geral" },
+    { value: "world", label: "Mundo" },
+    { value: "nation", label: "Nação" },
+    { value: "business", label: "Negócios" },
+    { value: "technology", label: "Tecnologia" },
+    { value: "entertainment", label: "Entreterimento" },
+    { value: "sports", label: "Esportes" },
+    { value: "science", label: "Ciência" },
+    { value: "health", label: "Saúde" }
+]
+
+const TopicNavigation = ({setCategory}) => {
+
+    return(
+        CATEGORIES.map((cat) => (
+            <Button
+                key = {cat.value}
+                id = {cat.value}
+                variant="primary"
+                width="full"
+                children={cat.label}
+                className="h-12"
+                onClick={() => {
+                    setCategory(cat.value);
+                }}
+                
+            />
+        ))
+    )
+}
+
+export default TopicNavigation
