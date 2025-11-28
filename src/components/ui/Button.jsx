9f9be7581114c3ee3af7381126861038d783bb9b
Fenelon02 <joaofenelonramossoares@gmail.com>
2025-11-28 12:25:54 -0300

    FIX: the designs

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,13 +1,14 @@
 const Button = (props) =>{
     const variants = {
         TopicNavigation:"rounded-b-xl text-white hover:bg-blue-700 focus:bg-blue-700 focus:rounded-b-xl hover:rounded-b-xl mr-2 px-2 h-8 w-15 flex flex-col items-center justify-center",
-        header:"rounded-lg bg-amber-100 mr-2 px-2 h-8 focus:shadow-lg focus:shadow-white hover:shadow-md hover:shadow-amber-100 flex flex-col items-center justify-center mb-2 md:mb-0",  
+        header:"text-white border border-white hover:bg-white hover:text-blue-600 p-2 rounded-lg transition-colors mb-2 md:mb-0",  
     }
     const width = {
         full: "w-full",
         small: "w-32",
         medium: "w-48",
-        large: "w-64"
+        large: "w-64",
+        header: "w-40"
     }
 
     const btnClasses = [
