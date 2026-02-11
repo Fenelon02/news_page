ca699d7c4354d0faca5481978c68f0746c724296
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-02-11 11:54:07 -0300

    CHORE: remove unused classes on the not found pages

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 const NotFoundPage = () =>{
     return(
         <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-6xl font-extrabold">Not found :´(</h1>
+            <h1 className="text-6xl font-extrabold">Not found</h1>
             <Link to={"/"} className="bg-blue-600 rounded-lg px-4 py-2 text-amber-50 mt-2">
                 <button className="font-semibold">Go back to the homepage</button>
             </Link>
