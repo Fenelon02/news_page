953660be1a32c9f98c5c94ab94008326de36a3ef
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-02-11 14:08:45 -0300

    Change the Header, HeaderForm and MfScreenForm to an specific folder, on the layout section

---
+++ src/components/layout/header/Header.jsx
@@ -0,0 +1,43 @@
+import logo from "../../../assets/logo.svg"
+import HeaderForm from "./HeaderForm";
+import TopicNavigation from "../../cards/notices/TopicNavigation"
+import { useEffect, useState } from "react";
+import MDScreenForm from "./MDScreenForm";
+
+
+const Header = ({onSearch}) => {
+    const [category, setCategory] = useState("general");
+    const [lang, setLang] = useState("pt");
+    const [country, setCountry] = useState("br");
+
+    useEffect(() => {
+        onSearch(lang, country, category);
+    }, [lang, country, category]);
+
+    return(
+        <header className="h-[5vh] sticky top-0">
+            <div className="grid grid-cols-3 bg-blue-700">
+               <div className="flex flex-row gap-2 ml-3 col-span-2">
+                   <img src={logo} alt="logo Diário 10" className="md:w-36 md:h-32 w-22 h-20 col-span-1"/>
+                    <div className="flex flex-col items-center justify-center">
+                        <h1 className="text-xl md:text-3xl lg:text-3xl text-[#E2E2B6] font-extrabold _font-family">Diário 10</h1>
+                    </div>
+               </div>
+
+                <div className="flex items-center justify-end mr-3 lg:mr-0">
+                    <div className="hidden lg:flex">
+                        <HeaderForm setCountry={ setCountry } setLang = { setLang }/>
+                    </div>
+                    <div className="flex lg:hidden">
+                        <MDScreenForm setLang={ setLang } setCountry={ setCountry } category={category} setCategory={setCategory}/>
+                    </div>
+                </div>
+            </div>
+            <div className="hidden bg-blue-500 mb-4 lg:grid lg:grid-cols-9 shadow-xl shadow-gray-300">
+                  <TopicNavigation category={category} setCategory={setCategory}/>  
+            </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/layout/header/HeaderForm.jsx
@@ -0,0 +1,65 @@
+import { useForm, FormProvider } from "react-hook-form";
+import Select from "../../ui/Select"
+import Button from "../../ui/Button";
+
+const HeaderForm = ({setCountry, setLang}) => {
+    const methods = useForm()
+    const { register } = methods;
+
+    const onSubmit = data => {
+        setCountry(data.country)
+        setLang(data.lang)
+    }
+
+    return(
+        <FormProvider {...methods}>
+            <form onSubmit={methods.handleSubmit(onSubmit)} className="flex flex-col md:flex-row items-center justify-center md:mr-4">
+                <Select
+                    options={
+                        [
+                            {label: "Português", value: "pt"},
+                            {label: "English", value: "en"},
+                            {label: "Español", value: "es"},
+                            {label: "Français", value: "fr"},
+                            {label: "Deutsch", value: "de"},
+                            {label: "中國人", value: "zh"},
+                            {label: "日本語", value: "ja"}
+                        ]
+                    }
+                    selectName="lang"
+                    required={true}
+                />
+                <Select
+                    options={
+                        [
+                            {label: "Brasil", value: "br"},
+                            {label: "EUA", value: "us"},
+                            {label: "España", value: "es"},
+                            {label: "France", value: "fr"},
+                            {label: "Deutschland", value: "de"},
+                            {label: "中國", value: "cn"},
+                            {label: "日本", value: "jp"}
+                        ]
+                    }
+                    selectName="country"
+                    required={true}
+                />
+
+                <Button
+                    type="submit"
+                    variant="header"
+                    width="header"
+                    className="md:w-full"
+                    children={
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                        </svg>
+                    }
+                />
+                    
+            </form>
+        </FormProvider>
+    )
+}
+
+export default HeaderForm
