bd16dbf89c4ab17cd9e2306bdfa4694fd8683fa5
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-02-11 13:52:59 -0300

    CHORE: fix the padding top on the loading inside the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,7 +29,7 @@ export default function Home() {
     return (
       <div>
         <Header onSearch={getNews}/>
-        <div className="flex flex-col justify-center items-center h-[80vh]">
+        <div className="flex flex-col justify-center items-center h-screen pt-[5vh] lg:pt-[9vh]">
           <h2 className="text-5xl text-blue-700 flex flex-center items-center">Carregando as melhores notícias para você!</h2>
         </div>
       </div>
