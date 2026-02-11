f8af290406c2e0fc6417de80fabee14e7aa40b21
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-02-11 14:07:41 -0300

    Change the newsBox, renderNews and TopicNavigation to an specific folder, the notices folder

---
+++ src/components/cards/notices/RenderNews.jsx
@@ -0,0 +1,35 @@
+import NewsBox from './news/NewsBox.js';
+
+const RenderNews = ({ news }) => {
+  if (news.length > 0) {
+    return(
+        <div className="m-8 pt-[5vh] lg:pt-[9vh]">
+            <div className='lg:grid lg:grid-cols-2'>
+              {news.map((article, index) => {
+                  if(index % 3 == 0){ 
+                    return (
+                      <NewsBox 
+                      article = {article} 
+                      key={article.id} 
+                      boxClassName={"lg:col-span-2"} />)
+                    } else {
+                      return (
+                        <NewsBox 
+                        article = {article} 
+                        key={article.id} 
+                        boxClassName={"lg:col-span-1"} />)
+                    }
+              })}
+            </div>
+        </div>
+    )
+  } else {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen pt-[5vh] lg:pt-[9vh]">
+        <h2 className="text-6xl text-blue-700">Sem noticias no momento.</h2>
+      </div>
+    );
+  }
+}
+
+export default RenderNews
---
+++ src/components/cards/notices/TopicNavigation.jsx
@@ -0,0 +1,47 @@
+import Button from "../../ui/Button";
+
+const CATEGORIES = [
+    { value: "general", label: "Geral" },
+    { value: "world", label: "Mundo" },
+    { value: "nation", label: "Nação" },
+    { value: "business", label: "Negócios" },
+    { value: "technology", label: "Tecnologia" },
+    { value: "entertainment", label: "Entreterimento" },
+    { value: "sports", label: "Esportes" },
+    { value: "science", label: "Ciência" },
+    { value: "health", label: "Saúde" }
+]
+
+const TopicNavigation = ({category, setCategory}) => {
+
+    return(
+        CATEGORIES.map((cat) => {
+            if(category === cat.value){
+               return(
+                <Button
+                key = {cat.value}
+                id = {cat.value}
+                variant="TopicNavigation"
+                width="full"
+                children={cat.label}
+                className="h-12 max-h-[4vh] bg-blue-700 border border-white md:border-0"
+                onClick={() => {
+                    setCategory(cat.value)}}/>  
+            )} else{
+                return(
+                    <Button
+                    key = {cat.value}
+                    id = {cat.value}
+                    variant="TopicNavigation"
+                    width="full"
+                    children={cat.label}
+                    className="h-12 max-h-[4vh]  bg-blue-500"
+                    onClick={() => {
+                        setCategory(cat.value)}}/>  
+                )
+            }
+        })
+    )
+}
+
+export default TopicNavigation
