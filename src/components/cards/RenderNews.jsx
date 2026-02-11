e1bd5ba91629427719b19f3957bb03e06ae0ccb7
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-02-11 13:53:44 -0300

    CHORE: fix the padding top and the display responsivity on the render news

--- src/components/cards/RenderNews.jsx
+++ src/components/cards/RenderNews.jsx
@@ -3,21 +3,21 @@ import NewsBox from './NewsBox.jsx';
 const RenderNews = ({ news }) => {
   if (news.length > 0) {
     return(
-        <div className="m-8">
-            <div className='md:grid md:grid-cols-2'>
+        <div className="m-8 pt-[5vh] lg:pt-[9vh]">
+            <div className='lg:grid lg:grid-cols-2'>
               {news.map((article, index) => {
                   if(index % 3 == 0){ 
                     return (
                       <NewsBox 
                       article = {article} 
                       key={article.id} 
-                      boxClassName={"col-span-2"} />)
+                      boxClassName={"lg:col-span-2"} />)
                     } else {
                       return (
                         <NewsBox 
                         article = {article} 
                         key={article.id} 
-                        boxClassName={"col-span-2 md:col-span-1"} />)
+                        boxClassName={"lg:col-span-1"} />)
                     }
               })}
             </div>
@@ -25,7 +25,7 @@ const RenderNews = ({ news }) => {
     )
   } else {
     return (
-      <div className="flex flex-col items-center justify-center h-[80vh]">
+      <div className="flex flex-col items-center justify-center h-screen pt-[5vh] lg:pt-[9vh]">
         <h2 className="text-6xl text-blue-700">Sem noticias no momento.</h2>
       </div>
     );
