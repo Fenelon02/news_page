1782627e52427f624709ed47a5f61328fac2330c
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-02-13 11:32:17 -0300

    CHORE: change the max heigth of the button on the topic navigation

--- src/components/cards/notices/TopicNavigation.jsx
+++ src/components/cards/notices/TopicNavigation.jsx
@@ -24,7 +24,7 @@ const TopicNavigation = ({category, setCategory}) => {
                 variant="TopicNavigation"
                 width="full"
                 children={cat.label}
-                className="h-12 max-h-[4vh] bg-blue-700 border border-white md:border-0"
+                className="h-12 max-h-[5vh] bg-blue-700 border border-white lg:border-0"
                 onClick={() => {
                     setCategory(cat.value)}}/>  
             )} else{
@@ -35,7 +35,7 @@ const TopicNavigation = ({category, setCategory}) => {
                     variant="TopicNavigation"
                     width="full"
                     children={cat.label}
-                    className="h-12 max-h-[4vh]  bg-blue-500"
+                    className="h-12 max-h-[5vh] bg-blue-500"
                     onClick={() => {
                         setCategory(cat.value)}}/>  
                 )
